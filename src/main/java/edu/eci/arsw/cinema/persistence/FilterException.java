b9f637b9ffbe8686f671a5273e4fff030aed71e9
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-02-28 18:54:00 -0500

    entrega inicial
    Co-authored-by: John <JhonhRge@gmail.com>

---
+++ src/main/java/edu/eci/arsw/cinema/persistence/FilterException.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.cinema.persistence;
+
+public class FilterException extends Exception{
+
+    public FilterException(String message) {
+        super(message);
+    }
+
+    public FilterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
